c5339a5fc49eebea6ad41a6f32439ea188b1231d
Mike Połtyn <mike@railslove.com>
2011-09-05 19:38:17 +0200

    deployment

--- yuno.rb
+++ yuno.rb
@@ -12,6 +12,8 @@ class Yuno < AwesomeBotFactory::Skill
   
   def reply
     image = "http://memecaptain.com/i?u=http%3A%2F%2Fmemecaptain.com%2F"
+
+    # Y U NO/BUT WHEN I DO
     case self.action.upcase
     when "Y U NO"
       image << "y_u_no.jpg"
@@ -19,8 +21,12 @@ class Yuno < AwesomeBotFactory::Skill
       image << "most_interesting.jpg"
     end
     image << "&tt=#{URI.encode_www_form_component(self.text_1)}" unless self.text_1.empty?
-    image << "&tb=#{URI.encode_www_form_component("#{self.action} #{self.text_2}")}#jpg"
+    image << "&tb=#{URI.encode_www_form_component("#{self.action} #{self.text_2}")}"
 
+    # ALL THE
+    image = "http://memecaptain.com/i?u=http%3A%2F%2Fmemecaptain.com%2Fhttp%3A%2F%2Ftroll.me%2Fimages%2Fx-all-the-things%2Fx-all-the-things.jpg&tt=#{URI.encode_www_form_component(self.body)}" if self.action == "ALL THE"
+
+    image << "#jpg"
     {:type => "TextMessage", :body => image}
   end
   
